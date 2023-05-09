b4e15c2274a1d7a6b8f18565f44138ae4872ab07
Roman Hlembitskiy <roman.hlembitskiy@hygge.software>
2023-05-09 16:10:16 +0300

    - question-form intermediate commit

--- src/app/shared/components/question-form/question-form.component.ts
+++ src/app/shared/components/question-form/question-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { FirestoreService } from "../../services/firestore.service";
 
@@ -8,7 +8,12 @@ import { FirestoreService } from "../../services/firestore.service";
     styleUrls: ['./question-form.component.scss']
 })
 export class QuestionFormComponent implements OnInit {
+    @Input() public heading: string = 'QUESTIONS?';
+    @Input() public subHeading?: string = '';
+    @Input() public withUpload?: boolean = false;
+
     public contactFormGroup!: FormGroup;
+
     public get fc() {
         return this.contactFormGroup.controls;
     }
@@ -24,12 +29,17 @@ export class QuestionFormComponent implements OnInit {
         this.contactFormGroup = new FormGroup({
             name: new FormControl(null, [Validators.required]),
             email: new FormControl(null, [Validators.required, Validators.email]),
-            question: new FormControl(null, [Validators.required])
+            question: new FormControl(null, [Validators.required]),
+            cv: new FormControl(null)
         })
     }
 
     public submitQuestionsForm(fg: FormGroup): void {
         if (fg.invalid) return;
         this.firestore.submitForm(fg.value).then(() => fg.reset())
+    }
+
+    showDragDrop() {
+
     }
 }
